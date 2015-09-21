e0a60a3fa3751cdd17543bc2ab454fc8b80f6a13
criptych <criptych@gmail.com>
2015-09-21 16:19:15 -0400

    work

--- src/engine.hpp
+++ src/engine.hpp
@@ -20,6 +20,19 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/**
+ *  A note on coordinates and angles:
+ *   o  Absolute coordinates are given in blocks, as 64-bit fixed-point values
+ *      with 8 fractional bits.
+ *   o  Chunk coordinates are given as above, in whole blocks with no fractional
+ *      bits.
+ *   o  Angles use a 256-point scale analogous to degrees, with -128 = 180deg,
+ *      reducing modular angle calculations to a mask operation and most
+ *      trigonometry to a simple lookup.
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+
 typedef uint64_t EntityID;
 
 typedef int64_t Coord;
@@ -324,16 +337,20 @@ public:
 typedef uint16_t PacketSize;
 
 /**
- * Packet type codes.
+ *  Packet type codes.
  *
- * Packet types 0-127 (0x00-0x7f) are reserved for internal use.
- * Packet types 128-255 (0x80-0xff) are available for custom implementations,
- * and are ignored by the default/internal handler.
+ *  Packet types 0-127 (0x00-0x7f) are reserved for internal use.
+ *  Packet types 128-255 (0x80-0xff) are available for custom implementations,
+ *  and are ignored by the default/internal handler.
  *
- * Parameters
- * -----
+ *  Many packets follow the scheme used by the Minecraft protocol, mainly to
+ *  support the same range of features.  In addition, new packets are available
+ *  to support custom game definitions and more flexible modding.
  *
- *  All multi-byte values are in network/big-endian order
+ *  Parameters
+ *  -----
+ *
+ *  All multi-byte values are in network/big-endian order.
  *
  *  Types:
  *      "int8" - signed 8-bit integer
@@ -344,76 +361,206 @@ typedef uint16_t PacketSize;
  *      "uint32" - unsigned 32-bit integer
  *      "int64" - signed 64-bit integer
  *      "uint64" - unsigned 64-bit integer
+ *      "type[n]" - array of 'type' with length 'n'
+ *      "type[]" - array of 'type' with varying length
+ *      "blob8" - uint8 (length) followed by uint8[length]
+ *      "blob16" - uint16 (length) followed by uint8[length]
+ *      "blob32" - uint32 (length) followed by uint8[length]
  *      "string" - NUL-terminated UTF-8 string with uint16 prefix
- *              (length in bytes, including NUL)
- *      "type[]" - array of 'type' with varying length, usually prefixed
- *              (so 'string' may be represented: uint16, uint8[])
+ *                 (equivalent to blob16; length includes NUL)
  */
+
 enum class PacketType : uint8_t {
-    /* string (message) */
-    PlayerChat,
 
-    /* string (sender), string (message) */
-    ServerChat,
-
-    /* (no payload) */
-    ServerInformationRequest,
-
-    /* uint32 (num players), uint32 (max players), uint32 (reserved flags),
-     * string (info message) */
-    ServerInformationResponse,
-
-    /* string (player name) */
+    /**
+     *  Initiate login to server. (Client -> Server)
+     *
+     *  Parameters:
+     *      uint8[16] (UUID)
+     *      string (player name)
+     */
     ServerLoginRequest,
 
-    /* uint8 (login result), string (message) */
+    /**
+     *
+     *  Parameters:
+     *      uint8 (login result), string (message)
+     */
     ServerLoginResponse,
 
-    /* blob (auth token) */
+    /**
+     *
+     *  Parameters:
+     *      blob (auth token)
+     */
     ServerAuthRequest,
     ServerAuthResponse,
 
-    /* (no payload) */
+    /**
+     *
+     *  Parameters:
+     *      (no payload)
+     */
     ServerLogout,
 
-    /* int64 (x), int64 (y), int64 (z), uint16 (size), uint8[] (data)
+    /**
+     *  Request server information. (Client -> Server)
+     *
+     *  Sends a request for information about the server.  The server should
+     *  reply with a ServerInformationResponse command.
+     *
+     *  Parameters:
+     *      none
+     */
+    ServerInformationRequest,
+
+    /**
+     *  Send server information. (Server -> Client)
+     *
+     *  Returns information about the server to the client.
+     *
+     *  Parameters:
+     *      uint32 (num players)
+     *      uint32 (max players)
+     *      uint32 (reserved flags)
+     *      string (info message)
+     */
+    ServerInformationResponse,
+
+    /**
+     *  Send player chat message. (Client -> Server)
+     *
+     *  The server may handle the message in any way.  Specifically, many
+     *  servers will likely interpret messages starting with '/' as commands.
+     *
+     *  Parameters:
+     *      string (message)
+     */
+    PlayerChat,
+
+    /**
+     *  Send server chat message. (Server -> Client)
+     *
+     *  Like PlayerChat, plus indicates the originator of the message.
+     *
+     *  Parameters:
+     *      string (sender), string (message)
+     */
+    ServerChat,
+
+    /**
+     *
+     *  Parameters:
+     *      int64 (x), int64 (y), int64 (z), blob16 (size, data)
      *      if size != 16384, data is deflate-compressed
-     *      if size == 0, chunk is empty */
+     *      if size == 0, chunk is empty
+     */
     ChunkSingle,
 
-    /* int64 (x), int64 (y), int64 (z), uint8 num, uint16 (size), uint8[] (data)
+    /**
+     *
+     *  Parameters:
+     *      int64 (x), int64 (y), int64 (z), uint8 num, blob16 (size, data)
      *      if size != 16384 * num, data is deflate-compressed
-     *      if size == 0, chunks are empty */
+     *      if size == 0, chunks are empty
+     */
     ChunkColumn,
 
-    /* (no payload) */
+    /**
+     *  Check Resource (Client -> Server)
+     *
+     *      Sends the timestamp of the client's version of the given resource.
+     *      If the client does not have the resource, set timestamp to zero.
+     *      If server decides the resource is outdated, it should send a
+     *      LoadResource command with the new version.
+     *
+     *
+     *  Parameters:
+     *      uint32 (id), uint64 (timestamp)
+     */
+    CheckResource,
+
+    /**
+     *  Server -> Client
+     *      Sends resource data to the client.  The client should cache this
+     *      data for future connections to the server.
+     *
+     *  Parameters:
+     *      uint32 (id), uint64 (timestamp), uint16 (type), blob32 (size, data)
+     *      resource format described elsewhere; depends on type
+     */
+    LoadResource,
+
+    /**
+     *
+     *  Parameters:
+     *      (no payload)
+     */
     PlayerSpawn,
 
-    /* uint16 (width), uint16 (height), uint32 (size), uint8[] (data)
-     *      if size != width * height, data is deflate-compressed
-     *      (uncompressed) data is in RGBA format, starting at 0,0
-     *      skin image layout is described elsewhere */
+    /**
+     *
+     *  Parameters:
+     *      uint32 (image id)
+     *      skin image layout is described elsewhere
+     */
     PlayerSkin,
 
-    /* int64 (x), int64 (y), int64 (z), int8 (pitch), int8 (yaw) */
+    /**
+     *
+     *  Parameters:
+     *      int64 (x), int64 (y), int64 (z), int8 (pitch), int8 (yaw)
+     */
     PlayerMoveTo,
 
-    /* int16 (dx), int16 (dy), int16 (dz) */
+    /**
+     *
+     *  Parameters:
+     *      int16 (dx), int16 (dy), int16 (dz)
+     */
     PlayerMove,
 
-    /* int8 (pitch), int8 (yaw) */
+    /**
+     *
+     *  Parameters:
+     *      int8 (pitch), int8 (yaw)
+     */
     PlayerLook,
 
-    /* uint64 (id), uint32 (type), uint32 (data) */
+    /**
+     *
+     *  Parameters:
+     *      uint64 (eid), uint32 (image id)
+     *      skin image layout is described elsewhere
+     */
+    EntitySkin,
+
+    /**
+     *
+     *  Parameters:
+     *      uint64 (id), uint32 (type), uint32 (data)
+     */
     EntitySpawn,
 
-    /* uint64 (id), int64 (x), int64 (y), int64 (z), int8 (pitch), int8 (yaw) */
+    /**
+     *
+     *  Parameters:
+     *      uint64 (id), int64 (x), int64 (y), int64 (z), int8 (pitch), int8 (yaw)
+     */
     EntityMoveTo,
 
-    /* uint64 (id), int16 (dx), int16 (dy), int16 (dz) */
+    /**
+     *
+     *  Parameters:
+     *      uint64 (id), int16 (dx), int16 (dy), int16 (dz)
+     */
     EntityMove,
 
-    /* uint64 (id), int8 (pitch), int8 (yaw) */
+    /**
+     *
+     *  Parameters:
+     *      uint64 (id), int8 (pitch), int8 (yaw)
+     */
     EntityLook,
 
     CustomPacket = 0x80,
